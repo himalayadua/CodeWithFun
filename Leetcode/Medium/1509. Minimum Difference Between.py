142760ee125bbd0980f4c766cb2fc6b48cc794b6
himalayadua <himalaya.dua@gmail.com>
2024-07-03 07:19:36 -0700

    jul03 c1

---
+++ Leetcode/Medium/1509. Minimum Difference Between.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        return 0 if len(nums)<=4 else nums.sort() or min(nums[len(nums)-4+i]-nums[i] for i in range(4))
+        
