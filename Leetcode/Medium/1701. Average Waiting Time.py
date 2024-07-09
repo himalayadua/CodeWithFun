601daa3b13bbfda81df3a2ab2bbbcc550983f1dc
himalayadua <himalaya.dua@gmail.com>
2024-07-09 08:08:47 -0700

    jul09 c1

---
+++ Leetcode/Medium/1701. Average Waiting Time.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        res, finish = 0, 0
+        for arrival, time in customers:
+            if finish > arrival:
+                finish += time 
+            else: finish = arrival + time
+            res += finish - arrival
+        return res / len(customers)        
